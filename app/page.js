5619f547e6de5642099b4b881376107261ffe7fe
Rahul Aryan <rahularyan@Rahuls-MacBook-Air.local>
2026-02-12 18:47:22 +0530

    remove the api call

--- app/page.js
+++ app/page.js
@@ -12,36 +12,36 @@ import CoreCapabilities from "../components/home/CoreCapabilities";
 import VarietySection from "../components/home/VarietySection"
 
 export default async function Home() {
-  const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+  // const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 
-  const res = await fetch(
-    `${STRAPI_URL}/api/solutions?populate=animationVideo&populate=iTwinity`,
-    { next: { revalidate: 10 } }
-  );
+  // const res = await fetch(
+  //   `${STRAPI_URL}/api/solutions?populate=animationVideo&populate=iTwinity`,
+  //   { next: { revalidate: 10 } }
+  // );
 
-  const json = await res.json();
+  // const json = await res.json();
 
-  // Find entry that has animationVideo
-  const animationEntry = json?.data?.find(
-    (item) => item?.animationVideo
-  );
+  // // Find entry that has animationVideo
+  // const animationEntry = json?.data?.find(
+  //   (item) => item?.animationVideo
+  // );
 
-  const animationPath = animationEntry?.animationVideo?.url;
+  // const animationPath = animationEntry?.animationVideo?.url;
 
-  const animationUrl = animationPath
-    ? `${STRAPI_URL}${animationPath}`
-    : null;
+  // const animationUrl = animationPath
+  //   ? `${STRAPI_URL}${animationPath}`
+  //   : null;
 
-  // Find entry that has iTwinity
-  const iTwinityEntry = json?.data?.find(
-    (item) => item?.iTwinity
-  );
+  // // Find entry that has iTwinity
+  // const iTwinityEntry = json?.data?.find(
+  //   (item) => item?.iTwinity
+  // );
 
-  const iTwinityPath = iTwinityEntry?.iTwinity?.url;
+  // const iTwinityPath = iTwinityEntry?.iTwinity?.url;
 
-  const iTwinityUrl = iTwinityPath
-    ? `${STRAPI_URL}${iTwinityPath}`
-    : null;
+  // const iTwinityUrl = iTwinityPath
+  //   ? `${STRAPI_URL}${iTwinityPath}`
+  //   : null;
 
   return (
     <>
